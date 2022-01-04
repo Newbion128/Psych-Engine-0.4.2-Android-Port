7bcaa64d765e4fd8cad7238a92b8e506adc7ee64
Newbion128 <97077104+Newbion128@users.noreply.github.com>
2022-01-04 13:25:48 +0900

    Add files via upload

---
+++ assets/preload/data/Bushwhack/modchart.lua
@@ -0,0 +1,39 @@
+function start(song)
+    
+end
+
+function update(elapsed)
+	
+end
+
+function beatHit(beat)
+
+end
+
+function stepHit(step)
+	
+end
+
+function keyPressed(key) -- left, down, up, right
+   
+end
+
+function playerTwoTurn()
+
+end
+
+function playerOneTurn()
+
+end
+
+function playerOneMiss(direction, songPos) --direction: 0 is left, 1 is up, 2 is down, 3 is right
+
+end
+
+function playerOneSing(direction, songPos) --direction: 0 is left, 1 is up, 2 is down, 3 is right
+
+end
+
+function playerTwoSing(direction, songPos) --direction: 0 is left, 1 is up, 2 is down, 3 is right
+
+end
